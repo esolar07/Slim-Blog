3bfe92a89292ff90a429648dee8946f6a4dc825b
esolar07 <esolar07@gmail.com>
2017-02-01 17:36:51 -0500

    fixed issue with db info not showing.

--- app/controllers/HomePageController.php
+++ app/controllers/HomePageController.php
@@ -12,8 +12,8 @@ class HomePageController extends Controller
 	public function showBlogTitle($request, $response){
 		
 		$users = $this->c->db->query("SELECT * FROM guest_list")->fetchAll(PDO::FETCH_CLASS, HomePageModel::class);
-		var_dump($users);
-		die();
-		return $this->c->view->render($response, 'home.twig');
+		
+		//var_dump($users);
+		return $this->c->view->render($response, 'home.twig', compact('users') );
 	}
 }
