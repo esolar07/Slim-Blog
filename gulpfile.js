8ac33d8b36781d302ece863b18aaee528c6cf96b
esolar07 <esolar07@gmail.com>
2017-02-13 17:35:41 -0500

    added sass gulp workflow

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer');
+	
+var autoprefixerOptions = {
+	browsers: ['last 2 versions'],
+    cascade: false
+};
+
+
+gulp.task('sass', function(){
+	gulp.src('/public/sass/style.scss')
+	.pipe(sass())
+	.pipe(autoprefixer(autoprefixerOptions))
+	.pipe(gulp.dest('/public/css'))
+});
+
+
+gulp.task("watch:sass", function(){
+	gulp.watch('./public/sass/**/*.scss', ['sass'])
+});
